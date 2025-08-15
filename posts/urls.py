7c7f1da08b5f142876615bc34c509c904c8c3014
Alisher <dadabaev027@gmail.com>
2025-08-15 23:24:13 +0500

    add detail

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name = "posts"
 urlpatterns = [
     path("", PostListView.as_view(), name="list"),
+    path("<int:pk>/", PostDetailView.as_view(), name="detail")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import *
+from django.views import generic
 # Create your views here.
 
 
-class PostListView(ListView):
+class PostListView(generic.ListView):
     model  = Post
     template_name = "List.html"
     context_object_name = "posts"
 
-    
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = "Detail.html"
+    context_object_name = "post"
+
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    template_name = "Create.html"
+    context_object_name = "post"
+
+
+
+
 
 
