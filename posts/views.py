16db4d25f1e866b3daefe633f2412ea8289122b3
Alisher <dadabaev027@gmail.com>
2025-08-15 22:37:33 +0500

    add views and urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import *
 # Create your views here.
+
+
+class PostListView(ListView):
+    model  = Post
+    template_name = "List.html"
+    context_object_name = "posts"
+
+    
+
+
