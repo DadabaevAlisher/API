6512e863793fd1a936f2d0248136453047b9e700
Alisher <dadabaev027@gmail.com>
2025-08-17 17:07:40 +0500

    add user

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework.serializers import ModelSerializer
 from posts.models import Post
+from django.contrib.auth.models import User
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
 
 
 class PostSerializer(ModelSerializer):
+    author = UserSerializer(many = True)
+
     class Meta:
         model = Post
         fields = [
